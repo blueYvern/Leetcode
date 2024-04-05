1cb130d93450e3a035ad25e487d8a3e07deda5f3
blueYvern <142216705+blueYvern@users.noreply.github.com>
2024-04-05 23:18:03 +0530

    Create Solution.java

---
+++ 1544/Java/Make The String Great/Solution.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String makeGood(String s) {
+        
+        StringBuffer sb = new StringBuffer(s); 
+        boolean isDeleted = false;
+        do{
+            if(s.length() <= 1){
+                return s;
+            }
+      
+            isDeleted = false;
+            for(int i=0;i<=sb.length()-2;i++){
+                if ((sb.charAt(i) - sb.charAt(i+1) == 32) || (sb.charAt(i+1) - sb.charAt(i) == 32)) {
+                    sb.delete(i,i+2);
+                    isDeleted=true;
+                    break;
+                }   
+            }
+
+        }while(isDeleted);
+        return sb.toString();    
+    }
+}
